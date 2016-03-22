d3cfa5dab884e4fbd894eebd04eb7f5c285daa99
HurtworldMap <rogernobrega@live.com>
2016-03-22 05:09:51 -0300

    Organize

--- assets/leaflet.plugins/Leaflet.Bookmarks-master/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Leaflet bookmarks plugin
- * @license MIT
- * @author Alexander Milevski <info@w8r.name>
- * @preserve
- */
-var L = global.L || require('leaflet');
-
-L.Control.Bookmarks = module.exports = require('./src/bookmarks');
